0ad3ea4ffa5cb037ff16fb19e4fd7370c79db70e
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-08-15 12:59:42 +0545

    Implemented Experience  model

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import SiteSetting, PersonalInfo
+from .models import SiteSetting, PersonalInfo, Experience
 
 # Register your models here.
 admin.site.register(SiteSetting)
 admin.site.register(PersonalInfo)
+admin.site.register(Experience)
--- base/models.py
+++ base/models.py
@@ -20,3 +20,13 @@ class PersonalInfo(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class Experience(models.Model):
+    job_title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.job_title} at {self.company}"
